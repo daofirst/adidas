388f8c7f1a4f49ea4c974782c0459e8ec73469b3
daofirst <13764767060@163.com>
2018-04-21 17:43:17 +0800

    1

--- ftp.sh
+++ ftp.sh
@@ -1,5 +1,6 @@
-git pull
-composer dump-autoload
-php artisan config:clear
-php artisan cache:clear
-php artisan queue:restart
+cd /data/www/adidas
+git fetch 2>&1
+composer dump-autoload 2>&1
+php artisan config:clear 2>&1
+php artisan cache:clear 2>&1
+php artisan queue:restart 2>&1
