12c375432d1abe7a95c0fe887f1e7cab7fa4a560
madongge <59341935@qq.com>
2018-04-23 13:22:04 +0800

    测试

--- app/helpers.php
+++ app/helpers.php
@@ -4,6 +4,8 @@ if (! function_exists('wechat_user')) {
 
     function wechat_user() {
 
+        dd(session('wechat.oauth_user.default'));
+
         return session('wechat.oauth_user.default');
 
     }
